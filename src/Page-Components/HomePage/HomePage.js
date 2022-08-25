91423d43d2ab57051fb0123bd24f3882a07a72ed
Zamin58 <zaminzulfiqar@googlemail.com>
2022-08-25 21:47:43 +0000

    Fourth Commit

--- src/Page-Components/HomePage/HomePage.js
+++ src/Page-Components/HomePage/HomePage.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import FeaturedFilms from './HomePage-Components/FeaturedFilms';
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <FeaturedFilms />
+    </div>
+  );
 }
 
 export default HomePage;
